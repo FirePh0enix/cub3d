22fdd0aba853ff0d3103fd92c2e30446e449ebeb
FirePh0enix <leonard@delbecq.net>
2024-05-23 17:03:34 +0200

    Done something probably

--- src/render/draw_walls.c
+++ src/render/draw_walls.c
@@ -6,7 +6,7 @@
 /*   By: ledelbec <ledelbec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/31 15:03:52 by ledelbec          #+#    #+#             */
-/*   Updated: 2024/05/22 16:18:37 by ledelbec         ###   ########.fr       */
+/*   Updated: 2024/05/23 15:27:46 by ledelbec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,6 +92,11 @@ void	r3d_draw_wall(t_r3d *r3d, t_wall *wall, t_light *lights, t_v2i min, t_v2i m
 	int				x;
 	int				y;
 
+	// min.x = 0;
+	// max.x = 1279;
+	// min.y = 0;
+	// max.y = 719;
+
 	x = min.x - 1;
 	while (++x < max.x + 1)
 	{
@@ -100,56 +105,49 @@ void	r3d_draw_wall(t_r3d *r3d, t_wall *wall, t_light *lights, t_v2i min, t_v2i m
 		y = min.y - 1;
 		while (++y < max.y + 1)
 		{
-			float	py = (1 - 2 * ((y * 1 + 0.5) / r3d->height)) * r3d->tan2_fov;
-			t_v3	ray = (v3(px, py, -1.0));
+			// float	py = (1 - 2 * ((y * 1 + 0.5) / r3d->height)) * r3d->tan2_fov;
+			// t_v3	ray = v3(px, py, -1.0);
 			// TODO rotate the ray with the camera
 
-			if (wall_test_intersection(r3d, wall, camera_pos, ray, &t, &uv, &pos,
-				wall->n, wall->inverse_position))
-				set_pixel(r3d, x, y, sample_texture(r3d, wall->img, uv), t, wall->n, pos, lights);
+			r3d->fb->color[x + y * r3d->fb->width] = hex(0x000000FF);
+			// if (wall_test_intersection(r3d, wall, camera_pos, ray, &t, &uv, &pos,
+			// 	wall->n, wall->inverse_position))
+			//	set_pixel(r3d, x, y, sample_texture(r3d, wall->img, uv), t, wall->n, pos, lights);
 		}
 	}
 }
 
 static bool	project_corners(t_r3d *r3d, t_wall wall, t_v2i *min, t_v2i *max)
 {
+	const float		half_size = WALL_SIZE / 2;
 	const t_mat4	mat = mat4_mul_mat4(wall.position, wall.rotation);
-	t_tri			tri;
+	t_v3		p0 = v3(-half_size,  half_size, 0.0);
+	t_v3		p1 = v3( half_size,  half_size, 0.0);
+	t_v3		p2 = v3( half_size, -half_size, 0.0);
+	t_v3		p3 = v3(-half_size, -half_size, 0.0);
 
-	tri.v0 = v3(-WALL_SIZE / 2, -WALL_SIZE / 2, 0.0);
-	tri.v1 = v3(WALL_SIZE / 2, -WALL_SIZE / 2, 0.0);
-	tri.v2 = v3(WALL_SIZE / 2, WALL_SIZE / 2, 0.0);
+	p0 = mat4_multiply_v3(mat, p0);
+	p1 = mat4_multiply_v3(mat, p1);
+	p2 = mat4_multiply_v3(mat, p2);
+	p3 = mat4_multiply_v3(mat, p3);
 
-	// Transform the vertices
-	tri.v0 = mat4_multiply_v3(mat, tri.v0);
-	tri.v1 = mat4_multiply_v3(mat, tri.v1);
-	tri.v2 = mat4_multiply_v3(mat, tri.v2);
+	p0 = mat4_multiply_v3(r3d->projection_matrix, p0);
+	p1 = mat4_multiply_v3(r3d->projection_matrix, p1);
+	p2 = mat4_multiply_v3(r3d->projection_matrix, p2);
+	p3 = mat4_multiply_v3(r3d->projection_matrix, p3);
 
-	// Project the vertices
-	tri.v0 = mat4_multiply_v3(r3d->projection_matrix, tri.v0);
-	tri.v1 = mat4_multiply_v3(r3d->projection_matrix, tri.v1);
-	tri.v2 = mat4_multiply_v3(r3d->projection_matrix, tri.v2);
+	p0.x = (1 + p0.x) * 0.5 * r3d->fb->width, p0.y = (1 + p0.y) * 0.5 * r3d->fb->height;
+	p1.x = (1 + p1.x) * 0.5 * r3d->fb->width, p1.y = (1 + p1.y) * 0.5 * r3d->fb->height;
+	p2.x = (1 + p2.x) * 0.5 * r3d->fb->width, p2.y = (1 + p2.y) * 0.5 * r3d->fb->height;
+	p3.x = (1 + p3.x) * 0.5 * r3d->fb->width, p3.y = (1 + p3.y) * 0.5 * r3d->fb->height;
 
-	// Convert from screen space to NDC then raster (in one go)
-	tri.v0.x = (1 + tri.v0.x) * 0.5 * r3d->fb->width, tri.v0.y = (1 + tri.v0.y) * 0.5 * r3d->fb->height;
-	tri.v1.x = (1 + tri.v1.x) * 0.5 * r3d->fb->width, tri.v1.y = (1 + tri.v1.y) * 0.5 * r3d->fb->height;
-	tri.v2.x = (1 + tri.v2.x) * 0.5 * r3d->fb->width, tri.v2.y = (1 + tri.v2.y) * 0.5 * r3d->fb->height;
+	min->x = fminf(p0.x, fminf(p1.x, fminf(p2.x, p3.x)));
+	max->x = fmaxf(p0.x, fmaxf(p1.x, fmaxf(p2.x, p3.x)));
+	min->y = fminf(p0.y, fminf(p1.y, fminf(p2.y, p3.y)));
+	max->y = fmaxf(p0.y, fmaxf(p1.y, fmaxf(p2.y, p3.y)));
 
-	int	min_x, max_x, min_y, max_y;
-
-	min_x = min3f(tri.v0.x, tri.v1.x, tri.v2.x);
-	max_x = max3f(tri.v0.x, tri.v1.x, tri.v2.x);
-	min_y = min3f(tri.v0.y, tri.v1.y, tri.v2.y);
-	max_y = max3f(tri.v0.y, tri.v1.y, tri.v2.y);
-
-	if (min_x >= r3d->fb->width || min_y >= r3d->fb->height || max_x < 0 || max_y < 0)
+	if (min->x >= r3d->width || min->y >= r3d->height || max->x < 0 || max->y < 0)
 		return (false);
-
-	min->x = min_x;
-	min->y = min_y;
-	max->x = max_x;
-	max->y = max_y;
-
 	return (true);
 }
 
