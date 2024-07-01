349dff1df3591581222dda4d949b6ef8ea997c26
Inf1n1ty75 <volkandu75@gmail.com>
2024-07-01 19:02:03 +0200

    more map

--- src/init_door.c
+++ src/init_door.c
@@ -6,7 +6,7 @@
 /*   By: vopekdas <vopekdas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 18:27:24 by vopekdas          #+#    #+#             */
-/*   Updated: 2024/07/01 18:29:11 by vopekdas         ###   ########.fr       */
+/*   Updated: 2024/07/01 19:01:23 by vopekdas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@ void	init_door(char **av, t_vars *v)
 {
 	if (!ft_strcmp(av[1], "maps/ac-origins.cub"))
 		v->door = tga_load_from_file("assets/textures/egypt/DRFRONT.tga",
+				&v->at);
+	else if (!ft_strcmp(av[1], "maps/m4tr1x.cub"))
+		v->door = tga_load_from_file("assets/textures/m4tr1x/COMP1B_4.tga",
+				&v->at);
+	else if (!ft_strcmp(av[1], "maps/volcano.cub"))
+		v->door = tga_load_from_file("assets/textures/cave/CEIL1_2.tga",
 				&v->at);
 	else
 		v->door = tga_load_from_file("assets/textures/DOOR2_4.tga", &v->at);
