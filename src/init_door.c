16d48ae9aaf75b0983400f76c01ffcf6627b2d8d
Inf1n1ty75 <volkandu75@gmail.com>
2024-07-07 17:02:59 +0200

    star wars map look like star wars

--- src/init_door.c
+++ src/init_door.c
@@ -6,7 +6,7 @@
 /*   By: vopekdas <vopekdas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 18:27:24 by vopekdas          #+#    #+#             */
-/*   Updated: 2024/07/01 19:01:23 by vopekdas         ###   ########.fr       */
+/*   Updated: 2024/07/07 17:01:06 by vopekdas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,17 @@
 
 void	init_door(char **av, t_vars *v)
 {
-	if (!ft_strcmp(av[1], "maps/ac-origins.cub"))
+	if (!ft_strcmp(av[1], "maps/bonus/ac-origins.cub"))
 		v->door = tga_load_from_file("assets/textures/egypt/DRFRONT.tga",
 				&v->at);
-	else if (!ft_strcmp(av[1], "maps/m4tr1x.cub"))
+	else if (!ft_strcmp(av[1], "maps/bonus/m4tr1x.cub"))
 		v->door = tga_load_from_file("assets/textures/m4tr1x/COMP1B_4.tga",
 				&v->at);
-	else if (!ft_strcmp(av[1], "maps/volcano.cub"))
+	else if (!ft_strcmp(av[1], "maps/bonus/volcano.cub"))
 		v->door = tga_load_from_file("assets/textures/cave/CEIL1_2.tga",
+				&v->at);
+	else if (!ft_strcmp(av[1], "maps/bonus/star_wars.cub"))
+		v->door = tga_load_from_file("assets/textures/star_wars/DOOR2_5.tga",
 				&v->at);
 	else
 		v->door = tga_load_from_file("assets/textures/DOOR2_4.tga", &v->at);
